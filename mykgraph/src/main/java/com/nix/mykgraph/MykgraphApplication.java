3c460f838b7e3b1179d41e2a5c02b441c5ddc93c
“nitinramachandran” <nitin.ramachandran@gmail.com>
2022-06-20 01:00:35 +0530

    Spring Boot changes committed

---
+++ mykgraph/src/main/java/com/nix/mykgraph/MykgraphApplication.java
@@ -0,0 +1,13 @@
+package com.nix.mykgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MykgraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MykgraphApplication.class, args);
+	}
+
+}
---
+++ mykgraph/src/test/java/com/nix/mykgraph/MykgraphApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nix.mykgraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MykgraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
